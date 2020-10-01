8883096565e0e3cd1b380136ffa452554333657c
Brady Logan <brady.drew.logan@gmail.com>
2020-10-01 12:06:19 -0500

    created StudentList and cleared out main

--- src/mapExercises.js
+++ src/mapExercises.js
@@ -23,3 +23,15 @@ export const getStudentNames = () => {
     <h2>Cohort 43</h2>
   </div>
 */
+
+
+export const StudentList = () => {
+  return students.map(student => {
+    return `
+      <div>
+        <h1>${student.firstName + " " + student.lastName}</h1>
+        <h2>Cohort ${student.cohort}</h2>
+      </div>
+    `
+  }).join("")
+}
