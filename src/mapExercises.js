c82607425170b386361e4936defef449e1ff93da
Brady Logan <brady.drew.logan@gmail.com>
2020-10-01 11:56:21 -0500

    created getStudentNames

--- src/mapExercises.js
+++ src/mapExercises.js
@@ -7,6 +7,12 @@ const instructors = useInstructors();
 // It should not accept any parameters`
 // It should return an array of strings. Each string should be the full name of the student
 
+export const getStudentNames = () => {
+  return students.map(student => {
+    return student.firstName + " " + student.lastName
+  })
+}
+
 // Export a function named StudentList
 // It should not accept any parameters
 // It should return a single HTMLString that contains a <div> element for every student that looks like the following:
